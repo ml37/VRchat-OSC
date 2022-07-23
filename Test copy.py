f67c8329bec57ee19909c3767e6de277f15ddd80
ml37 <56399130+ml37@users.noreply.github.com>
2022-07-23 22:26:31 +0900

    Create Test copy.py

---
+++ Test copy.py
@@ -0,0 +1,20 @@
+import argparse
+import random
+import time
+
+from pythonosc import udp_client
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip", default="127.0.0.1",
+      help="The ip of the OSC server")
+  parser.add_argument("--port", type=int, default=9000,
+      help="The port the OSC server is listening on")
+  args = parser.parse_args()
+
+  client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+time.sleep(1)
+client.send_message("/avatar/parameters/VRCEmote", 1)
+time.sleep(1)
+client.send_message("/avatar/parameters/VRCEmote", 0)
